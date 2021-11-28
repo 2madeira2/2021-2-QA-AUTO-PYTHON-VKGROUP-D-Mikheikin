34013c4be0261696f819f98fab0cc9294d702465
2madeira2 <82679557+2madeira2@users.noreply.github.com>
2021-11-28 23:07:33 +0300

    Homework 3 (#3)
    * Homework 3 init
    
    * Update requirements.txt
    
    * Fix code
    
    * Refactoring get_token and post_login methods

---
+++ homework_3/code/resources/locations.py
@@ -0,0 +1,4 @@
+CREATE_CAMPAIGN_LOCATION = '/api/v2/campaigns.json'
+GET_SEGMENT_ID_LOCATION = '/api/v2/remarketing/segments.json'
+DELETE_SEGMENT_BY_ID_LOCATION = '/api/v2/remarketing/segments/'
+CREATE_SEGMENT_LOCATION = '/api/v2/remarketing/segments.json'
